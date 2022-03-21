619edef17c763abd13594e69391befcfe1a97d8e
NazRudy <nazrudy2014@gmail.com>
2022-03-21 18:58:38 +0200

    Устранение замечаний 1. Реформат кода 2. Исправление названий переменных 3. По поводу двух фреймов, выбрал низкий апи, и свойство backsgrount не работает, вышел из положения таким образом.

--- app/src/main/java/com/naz/calculatorhomework/MainActivity.java
+++ app/src/main/java/com/naz/calculatorhomework/MainActivity.java
@@ -14,21 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView display=findViewById(R.id.display);
-        Button num1=findViewById(R.id.button1);
-        Button num2=findViewById(R.id.button2);
-        Button num3=findViewById(R.id.button3);
-        Button num4=findViewById(R.id.button4);
-        Button num5=findViewById(R.id.button5);
-        Button num6=findViewById(R.id.button6);
-        Button num7=findViewById(R.id.button7);
-        Button num8=findViewById(R.id.button8);
-        Button num9=findViewById(R.id.button9);
-        Button num0=findViewById(R.id.button0);
-        Button ADD=findViewById(R.id.buttonADD);
-        Button SUB=findViewById(R.id.buttonSUB);
-        Button MUL=findViewById(R.id.buttonMUL);
-        Button DIV=findViewById(R.id.buttonDIV);
-        Button DOT=findViewById(R.id.buttonDOT);
+        TextView display = findViewById(R.id.display);
+        Button num1 = findViewById(R.id.button1);
+        Button num2 = findViewById(R.id.button2);
+        Button num3 = findViewById(R.id.button3);
+        Button num4 = findViewById(R.id.button4);
+        Button num5 = findViewById(R.id.button5);
+        Button num6 = findViewById(R.id.button6);
+        Button num7 = findViewById(R.id.button7);
+        Button num8 = findViewById(R.id.button8);
+        Button num9 = findViewById(R.id.button9);
+        Button num0 = findViewById(R.id.button0);
+        Button badd = findViewById(R.id.buttonADD);
+        Button bsub = findViewById(R.id.buttonSUB);
+        Button bmul = findViewById(R.id.buttonMUL);
+        Button bdiv = findViewById(R.id.buttonDIV);
+        Button bdot = findViewById(R.id.buttonDOT);
     }
 }
